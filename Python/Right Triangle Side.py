5290ab948f8b618192a4c7aea6911146b56aa8ac
Edwin <108167137+Edwin-The-Weird@users.noreply.github.com>
2022-07-05 19:31:43 +0530

    added height and base calculations

--- Python/Right Triangle Side.py
+++ Python/Right Triangle Side.py
@@ -25,7 +25,30 @@ if which_side == "hypotenuse":
         c_height = height * height
         c_base = base * base
         cZero_hypo = c_height + c_base
-        print(math.sqrt(cZero_hypo)) 
+        c_hypo = math.sqrt(cZero_hypo)
+        print("Hypotenuse: " + c_hypo)
+
+elif which_side == "height":
+        hypo = int(input("Hypotenuse of the Triangle: "))
+        base = int(input("Base of the Triangle: "))
+
+        c_hypo = hypo * hypo
+        c_base = base * base
+        cZero_height = c_hypotenuse - c_base
+        c_height = math.sqrt(cZero_height)
+        print("Height: " + c_height)
+
+elif which_side == "base":
+        hypo = int(input("Hypotenuse of the Triangle: "))
+        height = int(input("Height Of the Triangle: "))
+
+        c_hypo = hypo * hypo
+        c_height = height * height
+        cZero_base = c_hypo - c_height
+        c_base = math.sqrt(cZero_base)
+        print("Base: " + c_base)
+
+
 
 
 
