6273230bcbc4cf21de6e3af774f9cb6783b9f2e9
Edwin <108167137+Edwin-The-Weird@users.noreply.github.com>
2022-07-05 18:46:39 +0530

    Create Right Triangle Side.py

---
+++ Python/Right Triangle Side.py
@@ -0,0 +1,25 @@
+import math
+
+which_side = str(input("""
+Which of the Sides do you want to Find
+
+        |\
+        | \
+        |  \ Hypotenuse
+Height  |   \
+        |    \
+        |     \
+        |______\
+          length
+
+
+"""))
+
+which_side = which_side.lower()
+
+
+hypotenuse
+length
+height
+
+
